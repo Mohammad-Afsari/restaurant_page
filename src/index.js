c1a9e11ea00e08bfe984848484f5c95e8e6fe145
Mo <mo2005@hotmail.co.uk>
2021-12-13 18:11:23 +0000

    Functionality added to modules rev01

--- src/index.js
+++ src/index.js
@@ -1,6 +1,56 @@
 import { pageLoad } from "./pageload"
-// import { homePage } from "./home";
-// import { menu } from "./menu"
+import {sideNavigationMenu} from "./pageload"
+import { homePage } from "./home";
+import { menu } from "./menu"
 import { contact } from "./contact"
 
-console.log("Yoooooooooooooooo")
+
+// Get elements
+const navElements = document.querySelectorAll('#navElements')
+const hamburgerElements = document.querySelectorAll('#hamburgerElements')
+const parallaxDiv = document.getElementById('tabs')
+const home = document.getElementById('home')
+const menuPage = document.getElementById('menuPage')
+const contactPage = document.getElementById('contact')
+const homeFooter = document.querySelector('.homeFooter')
+const menuFooter = document.querySelector('.menuFooter')
+const contactFooter = document.querySelector('.contactFooter')
+
+// Navbar driven content
+navElements.forEach(function(elements) {
+    elements.addEventListener('click', navigateContent)
+})
+
+
+// Hamburger menu driven contact
+hamburgerElements.forEach(function(elements) {
+    elements.addEventListener('click', navigateContent)
+
+})
+
+// Functionality between pages
+function navigateContent(e) {     
+    if(e.target.innerHTML === "Home") {
+        home.classList.add('active')
+        homeFooter.classList.add('active')
+        menuPage.classList.remove('active')
+        menuFooter.classList.remove('active')
+        contactPage.classList.remove('active')
+        contactFooter.classList.remove('active')
+    } else if (e.target.innerHTML === "Menu") {
+        home.classList.remove('active')
+        homeFooter.classList.remove('active')
+        menuPage.classList.add('active')
+        menuFooter.classList.add('active')
+        contactPage.classList.remove('active')
+        contactFooter.classList.remove('active')
+        parallaxDiv.scrollIntoView();
+    } else if (e.target.innerHTML === "Contact") {
+        home.classList.remove('active')
+        homeFooter.classList.remove('active')
+        menuPage.classList.remove('active')
+        menuFooter.classList.remove('active')
+        contactPage.classList.add('active')
+        contactFooter.classList.add('active')
+    }
+}
--- src/pageload.js
+++ src/pageload.js
@@ -1,11 +1,12 @@
 // Hamburger menu functionality using materialisecss
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
-});
+const sideNavigationMenu = (() => {
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, {});
+    });    
+})();
 
-
-
+// Page content 
 const pageLoad = (() => {
     // Obtain container
     const content = document.getElementById('content')
@@ -19,20 +20,20 @@ const pageLoad = (() => {
             <a href="index.html" class="brand-logo" id="logo"><i class="material-icons">local_dining</i></a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons" id="hamburger">menu</i></a>
         <ul class="left hide-on-med-and-down">
-            <li><a href="#!">Home</a></li>
-            <li><a href="#!">Menu</a></li>
-            <li><a href="#!">Contact</a></li>
+            <li><a id="navElements" href="#home">Home</a></li>
+            <li><a id="navElements" href="#menu">Menu</a></li>
+            <li><a id="navElements" href="#contact">Contact</a></li>
         </ul>
         </div>
     </nav>
 
-    <ul class="sidenav" id="mobile-demo">
-        <li><a href="#!">Home</a></li>
-        <li><a href="#!">Menu</a></li>
-        <li><a href="#!">Contact</a></li>
+    <ul class="sidenav sidenav-close" id="mobile-demo">
+        <li><a id="hamburgerElements" href="#home">Home</a></li>
+        <li><a id="hamburgerElements" href="#menu">Menu</a></li>
+        <li><a id="hamburgerElements" href="#contact">Contact</a></li>
     </ul>
     `
     content.appendChild(newDiv)
 })();
 
-export default pageLoad;
+export default { pageLoad, sideNavigationMenu};
